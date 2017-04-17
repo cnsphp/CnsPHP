1a2416b5bff5b8b8e3c0c2d0ab60b2221415e5ae
cnsphp <yao.softs@gmail.com>
2017-04-17 10:19:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include("inc/common.inc.php");
+
+//CnsPHP相对于index.php的目录位置
+$GLOBALS['CnsPHP_base'] = "CnsPHP";
+
+$GLOBALS['CnsPHP_db_host'] = "localhost";
+$GLOBALS['CnsPHP_db_port'] = "3306";
+$GLOBALS['CnsPHP_db_user'] = "user";
+$GLOBALS['CnsPHP_db_pass'] = "pass";
+$GLOBALS['CnsPHP_db_name'] = "dbname";
+
+include($GLOBALS['CnsPHP_base']."/Controller/Controller.php");
+(new QCourse\Controller())->init();
